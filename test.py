af2a49a8fbf6c404bef25ab75589b80a3b2e70c3
Mihail Kuzmin <testic1996@mail.ru>
2024-12-26 17:23:26 +0500

    Add logs

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
+import torch
 from ultralytics import YOLO
 from pathlib import Path
+import logging
+
 
 model = YOLO('runs/detect/train3/weights/best.pt')
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='logs/test.log',
+)
 
 input_dir = Path('dataset/images/val')
 
 for img in input_dir.iterdir():
     results = model(img,save=True)
-
-
+    logging.info(f'{img.name} {len(results[0])}')
